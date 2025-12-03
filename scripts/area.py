c84250190916fe3ae634367d5df7373d422b021b
Felipe-CF <prog.study.fcf@gmail.com>
2025-12-03 10:26:12 -0300

    fix: correção de arquivos para quebra do pylint

--- scripts/area.py
+++ scripts/area.py
@@ -4,6 +4,16 @@ def calculando_area(lado_a, lado_b):
 
     return area
 
+def duplicata():
+
+    print("Duplicata para quebrar")
+
+    x = 10 * 20
+
+    y = x + 50
+
+    return y
+
 
 if __name__ == '__main__':
     area = calculando_area(5, 3)
