00b3af5a61cf3fe5e1f833fce7d53033ff21852e
Felipe-CF <prog.study.fcf@gmail.com>
2025-12-03 10:28:14 -0300

    fix: remoção de trecho duplicado

--- scripts/area_duplicated.py
+++ scripts/area_duplicated.py
@@ -4,15 +4,6 @@ def calculando_area_do_quadrado(lado_a, lado_b):
 
     return area
 
-def duplicata():
-
-    print("Duplicata para quebrar")
-
-    x = 10 * 20
-
-    y = x + 50
-
-    return y
 
 if __name__ == '__main__':
     area = calculando_area_do_quadrado(5, 3)
