9b0a461bc5691950a5b396ccee1060efe3e9153d
Felipe-CF <prog.study.fcf@gmail.com>
2025-12-09 23:29:20 -0300

    fix: modelos (flake8)

--- clinica/backend/models/endereco.py
+++ clinica/backend/models/endereco.py
@@ -4,11 +4,13 @@ from django.db import models
 
 class Endereco(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    municipio = models.CharField(blank=False, null=False,)
-    bairro = models.CharField(blank=False, null=False,)
-    rua = models.CharField(blank=False, null=False,)
-    numero = models.CharField(blank=False, null=False,)
+    municipio = models.CharField(blank=False, null=False, max_length=255)
+    bairro = models.CharField(blank=False, null=False, max_length=255)
+    rua = models.CharField(blank=False, null=False, max_length=255)
+    numero = models.CharField(blank=False, null=False, max_length=20)
 
     def __str__(self):
-        return f"Endereco: {self.municipio}-{self.bairro}, {self.rua}, {self.numero}"
-
+        return (
+            f"Endereco: {self.municipio}-{self.bairro}, "
+            f"{self.rua}, {self.numero}"
+        )
--- clinica/backend/models/usuario.py
+++ clinica/backend/models/usuario.py
@@ -1,17 +1,17 @@
-import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class Usuario(AbstractUser):
-    PERFIL=(
+    PERFIL = (
         ('pro', 'Profissional'),
         ('pac', 'Paciente'),
         ('admin', 'Administrador'),
         ('atend', 'Atendente'),
     )
+
     perfil = models.CharField(max_length=20, choices=PERFIL)
     email = models.EmailField(blank=False, null=False)
 
     def __str__(self):
-        return self.username + ' ' + self.perfil
+        return f"{self.username} {self.perfil}"
